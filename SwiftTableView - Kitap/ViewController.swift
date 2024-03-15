24fea33900ee060547a190691e0fe33a7d5138ce
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-15 09:39:37 +0300

    Initial Commit

---
+++ SwiftTableView - Kitap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTableView - Kitap
+//
+//  Created by Aydın Kutlu on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
